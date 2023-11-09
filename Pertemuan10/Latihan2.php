57be5363d23593da6b36b61578d9ae86b248e75c
unknown <rikaratih26@gmail.com>
2023-11-09 15:38:49 +0700

    pertemuan 10

---
+++ Pertemuan10/Latihan2.php
@@ -0,0 +1,9 @@
+<?php 
+$a = 10;
+
+echo "Aku adalah angka " . $a . ".<br>";
+echo "Jika aku dikali 8, jumlahku sekarang " . $a * 8 . ".<br>";
+echo "Jika aku dibagi 4, jumlahku sekarang " . 80/4 . ".<br>";
+echo "Jika aku dikurang 6, jumlahku sekarang " . 20-6 . ".<br>";
+echo "Jika aku ditambah 2, jumlahku sekarang " . 14+2 . ".<br>";
+?>
