17934acc281983a7916e6e3cdf9eda229a112500
unknown <rikaratih26@gmail.com>
2023-12-22 14:20:08 +0700

    UAS PWEB no 4 dan no 5

---
+++ UASpweb/LuasPersegiPanjangRatih.php
@@ -0,0 +1,8 @@
+<?php
+$panjang = 10;
+$lebar = 6.5;
+$luas = $panjang*$lebar;
+echo "Panjang : 10 cm dan Lebar : 6,5 cm <br>";
+echo "Rumus luas persegi panjang = panjang X lebar <br>";
+echo " Jadi Luas Persegi Panjang adalah ".$luas;
+?>
